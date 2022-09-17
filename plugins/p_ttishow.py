4e06e7e58ed4bd5ed3cb1c11cfdde257062572d2
jikkubot32 <93936137+jikkubot32@users.noreply.github.com>
2022-09-17 12:30:45 +0400

    Update p_ttishow.py

--- plugins/p_ttishow.py
+++ plugins/p_ttishow.py
@@ -38,7 +38,7 @@ async def save_group(bot, message):
             return
         buttons = [[
             InlineKeyboardButton('ℹ️ Help', url=f"https://t.me/{temp.U_NAME}?start=help"),
-            InlineKeyboardButton('📢 Updates', url='https://t.me/FilmClub_Channel')
+            InlineKeyboardButton('📢 Updates', url='https://t.me/Film_ClubChannel')
         ]]
         reply_markup=InlineKeyboardMarkup(buttons)
         await message.reply_text(
